ff62bfe409f0e288c67c9ec8b5e16fffdfa1542f
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-05-27 10:50:47 -0400

    Rename meta box
    crop_location => image_position.
    location => position.

--- js/nova-resize-thumbnail-preview.js
+++ js/nova-resize-thumbnail-preview.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function($) {
     function updateThumbnailPreview() {
-        console.log("Updating thumbnail preview...");
-
         var thumbnail = $(".nrt-thumbnail");
         // Check if the thumbnail element exists
         if (thumbnail.length) {
@@ -10,16 +8,12 @@ jQuery(document).ready(function($) {
             var radioButtons = $("input[name='image_position']");
             var selectedPosition = "";
             radioButtons.each(function() {
-                console.log("Checking radio button...");
                 if ($(this).is(":checked")) {
-                    console.log("Radio button checked.");
                     selectedPosition = $(this).val();
                 }
             });
 
-            console.log("Selected position:", selectedPosition);
             thumbnail.css("background-position", selectedPosition.replace("-", " "));
-            console.log("Thumbnail preview updated.");
         } else {
             console.log("Thumbnail element not found.");
         }
--- nova-resize-thumbnail-metabox.php
+++ nova-resize-thumbnail-metabox.php
@@ -20,8 +20,8 @@ class NRT_Image_Position_Meta_Box {
     // Register the meta box
     public function register_meta_box() {
         add_meta_box(
-            'crop_location_meta_box',
-            __( 'Image Position', 'nrt' ), // Translatable string with text domain 'nrt'
+            'image_position_meta_box',
+            __( 'Image Position', 'nrt' ),
             array( $this, 'render_meta_box' ),
             'attachment',
             'side',
@@ -31,16 +31,16 @@ class NRT_Image_Position_Meta_Box {
 
     // Render the meta box
     public function render_meta_box( $post ) {
-        $crop_locations = array(
-            'left-top' => 'Left Top', 'right-top' => 'Right Top', 'center-top' => 'Center Top',
-            'left-center' => 'Left Center', 'right-center' => 'Right Center', 'center-center' => 'Center Center',
-            'left-bottom' => 'Left Bottom', 'right-bottom' => 'Right Bottom', 'center-bottom' => 'Center Bottom'
+        $image_position = array(
+            'left-top' => 'Left Top', 'center-top' => 'Center Top', 'right-top' => 'Right Top',
+            'left-center' => 'Left Center', 'center-center' => 'Center Center', 'right-center' => 'Right Center',
+            'left-bottom' => 'Left Bottom', 'center-bottom' => 'Center Bottom', 'right-bottom' => 'Right Bottom'
         );
 
         // Retrieve the current crop location if it exists
-        $current_crop_location = get_post_meta( $post->ID, 'image_position', true );
+        $current_image_position = get_post_meta($post->ID, 'image_position', true) ?: 'center-top';
 
-        wp_nonce_field( 'save_image_position', 'image_position_nonce' );
+        wp_nonce_field('save_image_position', 'image_position_nonce');
 
         // Output the meta box content
         echo '<div style="display: flex; flex-direction: column; align-items: center;">';
@@ -50,12 +50,12 @@ class NRT_Image_Position_Meta_Box {
 
         echo '<table align="center" style="border: 1px solid black; padding: 0px; border-collapse: collapse;">';
         $i = 0;
-        foreach ( $crop_locations as $location => $label ) {
-            $checked = ( $location === $current_crop_location ) ? 'checked' : '';
+        foreach ( $image_position as $position => $label ) {
+            $checked = ( $position === $current_image_position ) ? 'checked' : '';
             if ($i % 3 === 0) {
                 echo '<tr>';
             }
-            echo '<td style="border: 1px solid black; padding: 0px; margin: 0px; height: 85px; width: 85px; vertical-align: middle; text-align: center;"><label class="screen-reader-text"> ' . esc_html( $label ) . '</label><input type="radio" name="image_position" value="' . esc_attr( $location ) . '" ' . $checked . '></td>';
+            echo '<td style="border: 1px solid black; padding: 0px; margin: 0px; height: 85px; width: 85px; vertical-align: middle; text-align: center;"><label class="screen-reader-text"> ' . esc_html( $label ) . '</label><input type="radio" name="image_position" value="' . esc_attr( $position ) . '" ' . $checked . '></td>';
             $i++;
             if ($i % 3 === 0) {
                 echo '</tr>';
@@ -69,21 +69,23 @@ class NRT_Image_Position_Meta_Box {
     }
 
     // Save the selected crop location
-    public function save_meta_box_data( $post_id ) {
+    public function save_meta_box_data($post_id) {
         // Check if nonce is set and valid
-        if ( ! isset( $_POST['image_position_nonce'] ) || ! wp_verify_nonce( $_POST['image_position_nonce'], 'save_image_position' ) ) {
+        if (!isset($_POST['image_position_nonce'] ) || !wp_verify_nonce( $_POST['image_position_nonce'], 'save_image_position')) {
             return;
         }
 
         // Check if the current user has permission to edit the post
-        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        if (!current_user_can( 'edit_post', $post_id)) {
             return;
         }
 
         // Sanitize and save the crop location
-        if ( isset( $_POST['image_position'] ) ) {
-            $crop_location = sanitize_text_field( $_POST['image_position'] );
-            update_post_meta( $post_id, 'image_position', $crop_location );
+        if (isset($_POST['image_position'])) {
+            $crop_location = sanitize_text_field($_POST['image_position']);
+            update_post_meta($post_id, 'image_position', $crop_location);
+        } else {
+            update_post_meta($post_id, 'image_position', 'center-top');
         }
     }
 }
